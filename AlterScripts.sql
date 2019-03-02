dc4036d4604a564ef74cb42fb41fa3200795a9f2
Toshal Tambave <toshaltambave@gmail.com>
2019-03-02 02:46:33 -0600

    Create AlterScripts.sql

---
+++ AlterScripts.sql
@@ -0,0 +1,20 @@
+ALTER TABLE `parking_management`.`parking_spots` 
+ADD COLUMN `Spot_UID` INT NULL AUTO_INCREMENT AFTER `PermitType`,
+ADD UNIQUE INDEX `Spot_UID_UNIQUE` (`Spot_UID` ASC) VISIBLE;
+;
+
+
+CREATE TABLE `parking_management`.`reservations` (
+  `Reservation_Id` INT NOT NULL AUTO_INCREMENT,
+  `Spot_UID` INT NOT NULL,
+  `Start_Time` DATETIME NOT NULL,
+  `End_Time` DATETIME NOT NULL,
+  `NoShow` TINYINT NULL DEFAULT 0,
+  `OverStay` TINYINT NULL DEFAULT 0,
+  PRIMARY KEY (`Reservation_Id`, `Spot_UID`, `End_Time`, `Start_Time`),
+  INDEX `Fk_Spot_UID_idx` (`Spot_UID` ASC) VISIBLE,
+  CONSTRAINT `Fk_Spot_UID`
+    FOREIGN KEY (`Spot_UID`)
+    REFERENCES `parking_management`.`parking_spots` (`Spot_UID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
