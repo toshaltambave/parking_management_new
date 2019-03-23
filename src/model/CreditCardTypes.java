e6fcef683860059a84821acd326cf433f7a6cb30
jmelende <joel.melendez@mavs.uta.edu>
2019-03-22 20:17:07 -0500

    all coverage for my parts

--- src/model/CreditCardTypes.java
+++ src/model/CreditCardTypes.java
@@ -1,25 +1,6 @@
 package model;
 
 public enum CreditCardTypes {
-	VISA,
-	MASTERCARD,
-	DISCOVER,
-	AMEX;
+	VISA, MASTERCARD, DISCOVER, AMEX;
 
-	
-    private String creditcardtype;
-
-    private CreditCardTypes ( String s )
-    {
-    	creditcardtype = s;
-    }
-    
-    private CreditCardTypes()
-    {
-    }
-
-    public String getCreditCardTypes()
-    {
-        return creditcardtype;
-    }
 }
