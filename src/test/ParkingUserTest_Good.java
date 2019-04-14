b528afd62adc81688c8b95a5cd2b3459b0391ac5
Toshal Tambave <toshaltambave@gmail.com>
2019-04-13 22:32:25 -0500

    Update ParkingUserTest_Good.java

--- src/test/ParkingUserTest_Good.java
+++ src/test/ParkingUserTest_Good.java
@@ -12,6 +12,8 @@ import org.openqa.selenium.support.ui.Select;
 
 import data.UsersDAO;
 import functions.BusinessFunctions;
+import model.Users;
+import util.PasswordUtility;
 
 public class ParkingUserTest_Good extends BusinessFunctions {
   private WebDriver driver;
@@ -53,8 +55,78 @@ public class ParkingUserTest_Good extends BusinessFunctions {
   		functions.RegisterUserDetails(driver, "Toshal", "Tam", "Male", "1", "LexCorp", "Lex@aol.com", "4693332514", "14412552", "30", "12332147", "1000212003");
   	}
     driver.get(appUrl);
+    
+    //UnHappy
+    functions.Login(driver, "T", "");
+    Users checkUser = new Users(new UsersDAO());
+    String mySecurePassword = PasswordUtility.generatePassword("");
+    UsersDAO.userExists("T", mySecurePassword, checkUser);
+    if(checkUser.getUserID() == null){
+    	assertEquals("username or password is incorrect.", driver.findElement(By.id("usernameError")).getAttribute("value"));
+    	assertEquals("Login Failed.", driver.findElement(By.id("errorMsg")).getAttribute("value"));
+    }
+    else{
+    	try {
+          assertEquals("PARKING USER HOMEPAGE", driver.findElement(By.cssSelector("h2")).getText());
+        } catch (Error e) {
+          verificationErrors.append(e.toString());
+        }
+    	
+    }
+    //UnHappy2
+    functions.Login(driver, "Tester1234", "1");
+    checkUser = new Users(new UsersDAO());
+    mySecurePassword = PasswordUtility.generatePassword("1");
+    UsersDAO.userExists("Tester1234", mySecurePassword, checkUser);
+    if(checkUser.getUserID() == null){
+    	assertEquals("username or password is incorrect.", driver.findElement(By.id("usernameError")).getAttribute("value"));
+    	assertEquals("Login Failed.", driver.findElement(By.id("errorMsg")).getAttribute("value"));
+    }
+    else{
+    	try {
+          assertEquals("PARKING USER HOMEPAGE", driver.findElement(By.cssSelector("h2")).getText());
+        } catch (Error e) {
+          verificationErrors.append(e.toString());
+        }
+    	
+    }
+    //UnHappy3
+    functions.Login(driver, "Tester1234", "Tester1234");
+    checkUser = new Users(new UsersDAO());
+    mySecurePassword = PasswordUtility.generatePassword("Tester1234");
+    UsersDAO.userExists("Tester1234", mySecurePassword, checkUser);
+    if(checkUser.getUserID() == null){
+    	assertEquals("username or password is incorrect.", driver.findElement(By.id("usernameError")).getAttribute("value"));
+    	assertEquals("Login Failed.", driver.findElement(By.id("errorMsg")).getAttribute("value"));
+    }
+    else{
+    	try {
+          assertEquals("PARKING USER HOMEPAGE", driver.findElement(By.cssSelector("h2")).getText());
+        } catch (Error e) {
+          verificationErrors.append(e.toString());
+        }
+    	
+    }
+    
+    //Happy
     functions.Login(driver, "toshaltest", "Toshal123.");
-    functions.makeReservation(driver, "2019-04-13 21:00:00", "2019-04-13 22:00:00", "Nedderman", "Basic", 1 , 3, "4238000023456780", "12", "2020", "213");
+    checkUser = new Users(new UsersDAO());
+    mySecurePassword = PasswordUtility.generatePassword("Toshal123.");
+    UsersDAO.userExists("toshaltest", mySecurePassword, checkUser);
+    if(checkUser.getUserID() == null){
+    	assertEquals("username or password is incorrect.", driver.findElement(By.id("usernameError")).getAttribute("value"));
+    	assertEquals("Login Failed.", driver.findElement(By.id("errorMsg")).getAttribute("value"));
+    }
+    else{
+    	try {
+          assertEquals("PARKING USER HOMEPAGE", driver.findElement(By.cssSelector("h2")).getText());
+        } catch (Error e) {
+          verificationErrors.append(e.toString());
+        }
+    	
+    }
+    //functions.Login(driver, "toshaltest", "Toshal123.");
+    functions.makeReservation(driver, "2019-04-13 23:00:00", "2019-04-13 23:15:00", "Nedderman", "Basic", 1 , 3, "4238000023456780", "12", "2020", "213");
   }
 
   @After
